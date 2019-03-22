f3805001a8e55d887bbcaf3eec24ae3587f2c666
juniarsofyan <choerulsofyanmf@gmail.com>
2019-03-22 16:55:37 +0700

    shopping cart remove item

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -35,6 +35,31 @@ class Cart extends CI_Controller
             $this->session->set_userdata('cart', $current_cart_items);
         }
     }
+
+    public function remove()
+    {
+        $product_id = $this->input->get('product_id');
+
+        if (!isset($this->session->cart)) {
+            return;
+        } else {
+            $current_cart_items = $this->session->cart;
+
+            foreach ($current_cart_items as $key => $item) {
+                if ($item['product_id'] == $product_id) {
+                    if ($item['qty'] == 0) {
+                        unset($current_cart_items[$key]);
+                        $this->session->set_userdata('cart', $current_cart_items);
+                        return;
+                    } else {
+                        $current_cart_items[$key]['qty'] -= 1;
+                        $this->session->set_userdata('cart', $current_cart_items);
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* End of file Cart.php */
